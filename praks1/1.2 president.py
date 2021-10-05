82e60fca4576f6627ad7c7c27a6c5ef075e6a9d7
Hanna Mägi <hanna.magi.001@kgk.ee>
2021-10-05 13:37:38 +0300

    1.2 President

---
+++ praks1/1.2 president.py
@@ -0,0 +1,8 @@
+#defineerin muutujad ja omistan väärtused
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause_keskosa = ". aastal valiti Eesti presidendiks "
+
+#muutujate kasutamine
+lause = str(aasta)+ lause_keskosa + president
+print(lause)
