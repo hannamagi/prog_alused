8ab23351ef52855cc8072ed6e0b15a4ff7c04e3b
Hanna Mägi <hanna.magi.001@kgk.ee>
2021-10-21 11:24:56 +0300

    1.3 Astendamine

---
+++ praks1/1.3 Astendamine.py
@@ -0,0 +1,7 @@
+#kasutaja sisend
+astme_alus = int(input("Sisesta astme alus: "))
+astendaja = int(input("Sisesta astendaja: "))
+#arvutused
+tulemus = astme_alus ** astendaja
+#tulemuse väljastamine
+print(tulemus)
