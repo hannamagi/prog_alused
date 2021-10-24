d9dcebac74ac2bf4e8f3f826ec306847cb4390b6
Hanna Mägi <hanna.magi.001@kgk.ee>
2021-10-24 21:43:16 +0300

    1.6 Trantspordi organiseerimine. py

---
+++ praks1/1.6 Trantspordi organiseerimine.py
@@ -0,0 +1,5 @@
+inimesed = int(input("Sisestage inimeste arv: "))
+kohad_ühes_bussis = int(input("Sisestage kohtade arv ühes bussis"))
+
+bussid = inimesed // kohad_ühes_bussis
+maha_jäänud = inimesed % kohad_ühes_bussis
