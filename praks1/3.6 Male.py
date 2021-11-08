23478250bba206bb4e2b66b182324f0cfe93a216
Hanna Mägi <hanna.magi.001@kgk.ee>
2021-11-08 16:58:34 +0200

    3.6 Male

---
+++ praks1/3.6 Male.py
@@ -0,0 +1,13 @@
+täisarv = int(input("Sisestage täisarv: "))
+i = 1
+nisuterad = 0
+
+while i <= täisarv:
+    if i == 1:
+        nisuterad = 1
+    else:
+        nisuterad = nisuterad * 2
+    i = i + 1
+    print(nisuterad)
+    
+print("Nisuteri " + str(täisarv) + ". ruudu eest: " + str(nisuterad))
